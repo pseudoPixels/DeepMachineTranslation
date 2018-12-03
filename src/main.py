b843e3cd88107acd2e922362ea4cb3b9cac2e3df
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-12-03 16:27:09 -0600

    main file updated

--- src/main.py
+++ src/main.py
@@ -34,10 +34,7 @@ dataset_toPairs = obj_dataPrepUtils.to_pairs(dataset)
 dataset_clean = obj_dataPrepUtils.clean_pairs(dataset_toPairs)
 
 #pickle save the cleaned dataset
-obj_fileIO.pickle_dump_data(dataset_clean, '../Datasets/deu-eng/english-german.pkl')
-
-
-#print(dataset_clean)
+#obj_fileIO.pickle_dump_data(dataset_clean, '../Datasets/deu-eng/english-german.pkl')
 
 
 
@@ -45,24 +42,4 @@ obj_fileIO.pickle_dump_data(dataset_clean, '../Datasets/deu-eng/english-german.p
 
 
 
-
-
-
-
-
-
-
-#instantiating the required objects.
-obj_fileIO = FileIoUtils()
-obj_dataPrepUtils = DataPreprocessingUtils()
-
-
-
-dataset = obj_fileIO.load_doc("../Datasets/deu-eng/deu.txt")
-dataset_toParis = obj_dataPrepUtils.to_pairs(dataset)
-
-
-
-
-print(dataset_toParis[0])
 
