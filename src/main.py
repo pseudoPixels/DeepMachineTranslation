bb3962a73b8d52af96b09b5bbfa9221dabd09eb7
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-12-03 20:01:03 -0600

    saved train test dataset for later use

--- src/main.py
+++ src/main.py
@@ -22,27 +22,34 @@ obj_dataPrepUtils = DataPreprocessingUtils()
 #load dataset...
 #======>>> Bengali 2 English: "../Datasets/ben-eng/ben.txt"
 #======>>> German 2 English: "../Datasets/deu-eng/deu.txt"
-dataset = obj_fileIO.load_doc("../Datasets/deu-eng/deu.txt")
+##dataset = obj_fileIO.load_doc("../Datasets/deu-eng/deu.txt")
 
 
 
 #transform the dataset in pairs...
-dataset_toPairs = obj_dataPrepUtils.to_pairs(dataset)
+##dataset_toPairs = obj_dataPrepUtils.to_pairs(dataset)
 
 
 #clean the datasets (removing punctuations, non-printable and so on)
-dataset_clean = obj_dataPrepUtils.clean_pairs(dataset_toPairs)
+##dataset_clean = obj_dataPrepUtils.clean_pairs(dataset_toPairs)
 
 #pickle save the cleaned dataset
 #obj_fileIO.pickle_dump_data(dataset_clean, '../Datasets/deu-eng/english-german.pkl')
 
 
 #load pickle dumped dataset
-raw_dataset = obj_fileIO.load_pickle_dump_dataset('../Datasets/deu-eng/english-german.pkl')
+#raw_dataset = obj_fileIO.load_pickle_dump_dataset('../Datasets/deu-eng/english-german.pkl')
 
 
-obj_dataPrepUtils.train_test_split(raw_dataset[:50, :], 0.5)
+# reduce dataset size
+##n_sentences = 100
 
+
+
+##trainDataset, testDataset = obj_dataPrepUtils.train_test_split(raw_dataset[:n_sentences, :], 0.9)
+
+##obj_fileIO.pickle_dump_data(trainDataset, '../Datasets/deu-eng/english-german-train.pkl')
+##obj_fileIO.pickle_dump_data(testDataset, '../Datasets/deu-eng/english-german-test.pkl')
 
 
 
